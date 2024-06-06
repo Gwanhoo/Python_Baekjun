bdfbd2c8f7e4e76315078e9c7344806f17cba107
Gwanhoo <kimgwanho0206@gmail.com>
2024-06-07 03:57:08 +0900

    [Silver IV] Title: 카드2, Time: 216 ms, Memory: 50972 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+N = int(input())
+
+N_list = deque(i for i in range(1, N+1))
+
+while len(N_list) != 1:
+    N_list.popleft()
+    N_list.rotate(-1)
+
+print(N_list[0])
